1d225845bf4c8d554ad1b314d097b143c1c92290
Manuel Cañete <mcanete.plainconcepts@outlook.com>
2020-01-26 22:31:51 +0100

    Add prometheus and grafana

--- core/deployment/infrastructure/aks/provisioning.tf
+++ core/deployment/infrastructure/aks/provisioning.tf
@@ -28,3 +28,19 @@ resource "helm_release" "nginx_ingress" {
     "${file("provisioning/ingress.yaml")}"
   ]
 }
+
+resource "helm_release" "prometheus" {
+  name       = "prometheus"
+  namespace  = "monitor"
+  repository = data.helm_repository.stable.metadata[0].name
+  chart      = "stable/prometheus"
+  version    = "10.3.1"
+}
+
+resource "helm_release" "grafana" {
+  name       = "grafana"
+  namespace  = "monitor"
+  repository = data.helm_repository.stable.metadata[0].name
+  chart      = "stable/grafana"
+  version    = "4.4.0"
+}
